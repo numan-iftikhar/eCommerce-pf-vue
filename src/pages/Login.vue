222ba90a3fbe8ddfaa0248f1ace749b9eebc72c5
Numan Iftikhar <numaniftikhar300@gmail.com>
2023-01-13 16:22:45 +0500

    change login form

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,44 +1,103 @@
 <template>
+  <!-- login form start -->
+
   <div class="password-hint text-black mt-5 ml-2">
     <p><strong>username: </strong>kminchelle</p>
     <p><strong>password: </strong>0lelplR</p>
   </div>
-  <div class="my-10 d-flex justify-content-center align-items-center">
+  <section @submit.prevent="validateUser" class="dark:bg-gray-900">
     <div
-      class="col-md-5 p-5 shadow-sm border rounded-5 border-primary bg-white"
+      class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
     >
-      <h2 class="text-center mb-4 text-primary">Login Form</h2>
-      <form @submit.prevent="validateUser">
-        <div class="mb-3">
-          <label for="username" class="form-label">username</label>
-          <input
-            v-model="username"
-            type="text"
-            class="form-control border border-primary"
-            id="username"
-            aria-describedby="userHelp"
-          />
-        </div>
-        <div class="mb-3">
-          <label for="password" class="form-label">Password</label>
-          <input
-            v-model="password"
-            type="password"
-            class="form-control border border-primary"
-            id="password"
-          />
-        </div>
-        <div class="d-grid">
-          <button
-            class="btn bg-pink-400 text-white hover:bg-pink-500"
-            type="submit"
+      <div
+        class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+      >
+        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1
+            class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
           >
-            Login
-          </button>
+            Sign in to your account
+          </h1>
+          <form
+            @submit.prevent="validateUser"
+            class="space-y-4 md:space-y-6"
+            action="#"
+          >
+            <div>
+              <label
+                for="username"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Username</label
+              >
+              <input
+                v-model="username"
+                type="username"
+                name="username"
+                id="username"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="username"
+                required=""
+              />
+            </div>
+            <div>
+              <label
+                for="password"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >Password</label
+              >
+              <input
+                v-model="password"
+                type="password"
+                name="password"
+                id="password"
+                placeholder="password"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required=""
+              />
+            </div>
+            <div class="flex items-center justify-between">
+              <div class="flex items-start">
+                <div class="flex items-center h-5">
+                  <input
+                    id="remember"
+                    aria-describedby="remember"
+                    type="checkbox"
+                    class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                  />
+                </div>
+                <div class="ml-3 text-sm">
+                  <label for="remember" class="text-gray-500 dark:text-gray-300"
+                    >Remember me</label
+                  >
+                </div>
+              </div>
+              <a
+                href="#"
+                class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >Forgot password?</a
+              >
+            </div>
+            <button
+              type="submit"
+              class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              Sign in
+            </button>
+            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+              Don't have an account yet?
+              <a
+                href="#"
+                class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >Sign up</a
+              >
+            </p>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
-  </div>
+  </section>
+
+  <!-- login form end -->
 </template>
 <script>
 import { authenticateUser } from "@/services/service";
@@ -52,16 +111,6 @@ export default {
   },
   methods: {
     async validateUser() {
-      // const res = await fetch("https://dummyjson.com/auth/login", {
-      //   method: "POST",
-      //   headers: { "Content-Type": "application/json" },
-      //   body: JSON.stringify({
-      //     username: this.username,
-      //     password: this.password,
-      //     // expiresInMins: 60
-      //   }),
-      // });
-      // const data = await res.json();
       authenticateUser(this.username, this.password).then((data) => {
         this.$store.dispatch("addUser", data.token);
         localStorage.setItem("token", JSON.stringify(data.token));
